92da204f56832e10a0b5716ea9857cf505b2128e
Truphile <salidmonreal@gmail.com>
2026-02-27 11:10:13 +0100

    new commit

---
+++ wallet/utility.py
@@ -0,0 +1,11 @@
+import random
+import re
+
+from django.db.models.query_utils import refs_expression
+
+
+def generate_account_number():
+   return "44" + str(random.randrange(000000,999999))
+
+def generate_reference_id():
+   return "NKY" + str(random.shuffle([9000]))
