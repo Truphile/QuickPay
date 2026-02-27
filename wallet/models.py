20907cb17460bac88f4c60df195f37143e8fa3f4
Truphile <salidmonreal@gmail.com>
2026-02-27 20:57:59 +0100

    pay commit

--- wallet/models.py
+++ wallet/models.py
@@ -16,7 +16,7 @@ class Wallet(models.Model):
     )
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True, blank=True)
-    wallet_number = models.CharField(max_length=10, unique=True, primary_key=True)
+    wallet_number = models.CharField(max_length=10, unique=True, primary_key=True )
     account_number = models.CharField(max_length=10, unique=True,blank=True, default=generate_account_number)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='NGN')
