66903dd6e28fb32895cd4f65ba69d278efb38d54
Truphile <salidmonreal@gmail.com>
2026-02-27 15:17:40 +0100

    new commit

--- services/onboarding_service.py
+++ services/onboarding_service.py
@@ -3,7 +3,7 @@ from user.services import create_user
 from wallet.services import create_wallet
 
 
-
+@transaction.atomic
 def create_user_and_wallet(validated_data):
     user = create_user(validated_data)
     wallet = create_wallet(user)
