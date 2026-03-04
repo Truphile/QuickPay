159db406495451f18f6c3bb9cc59c4506382fb53
Truphile <salidmonreal@gmail.com>
2026-03-04 12:04:40 +0100

    pay commit

--- notification/services.py
+++ notification/services.py
@@ -4,12 +4,20 @@ from django.core.mail import send_mail
 
 
 def create_notification(user):
-    Notification.objects.create(user=user,
+    notification = Notification.objects.create(user=user,
                                 message=f"""
                                 Hi {user.first_name} Welcome to Quickpay!
-                                Your wallet number is : {user.wallet_number}
+                                Your wallet number is : {user.wallet.wallet_number}
                                 Your alternate wallet number is : {user.wallet.account_number}
-""")
+                                
+                                Thank you for choosing QuickPay!
+                                
+                                
+""",
+                                event_type='USER_WALLET_CREATED',
+
+
+                        )
 
     send_mail(
         subject="Welcome to Quickpay",
@@ -21,3 +29,7 @@ def create_notification(user):
 
     notification.is_read = True
     notification.save()
+    notification.message
+
+
+
