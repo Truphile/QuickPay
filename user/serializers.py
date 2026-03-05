62b12a82a0b88e8f149e0396d0517fdd8de923a7
Truphile <salidmonreal@gmail.com>
2026-03-05 04:36:28 +0100

    pay commit

--- user/serializers.py
+++ user/serializers.py
@@ -16,3 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create(**validated_data)
         wallet.objects.create(user=user, wallet_name=user.phone[1:])
         return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=100)
