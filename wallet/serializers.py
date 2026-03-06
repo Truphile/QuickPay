70e8fa8451236135395725dd89375e45b4bece1c
Truphile <salidmonreal@gmail.com>
2026-03-06 14:47:38 +0100

    pay commit

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -5,3 +5,9 @@ class WalletTransferSerializer(serializers.ModelSerializer):
     recipient_wallet = serializers.CharField(max_length=10)
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     idempotency_key = serializers.UUIDField(read_only=True)
+
+    def validate_amount(self, value):
+        if value < 0:
+            raise Exception("Amount cannot be negative")
+        return value
+
