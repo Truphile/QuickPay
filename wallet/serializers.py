36c9265ea4d1e4e6027350e346daeb8b825b6705
Truphile <salidmonreal@gmail.com>
2026-03-06 14:45:11 +0100

    pay commit

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 
 class WalletTransferSerializer(serializers.ModelSerializer):
-    recipient = serializers.CharField(max_length=10)
+    recipient_wallet = serializers.CharField(max_length=10)
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     idempotency_key = serializers.UUIDField(read_only=True)
