03c245228a26dfbfeb2a9fd34b6bed4c3291b465
Truphile <salidmonreal@gmail.com>
2026-03-17 10:58:42 +0100

    pay commit

---
+++ wallet/views.py
@@ -0,0 +1,60 @@
+from rest_framework import status
+from rest_framework.views import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from wallet.services.deposit import deposit
+from .models import Wallet
+from .serializers import WalletTransferSerializer, DepositSerializer
+from wallet.services.intra_transfer_service import transfer_wallet_to_wallet
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def transfer_wallet(request):
+    sender = request.user.wallet
+    serializer = WalletTransferSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    amount = serializer.validated_data['amount']
+    idempotency_key = serializer.validated_data['idempotency_key']
+    description = serializer.validated_data['description']
+    recipient = get_object_or_404(Wallet, wallet_number=serializer.validated_data['recipient_wallet'])
+    tx = transfer_wallet_to_wallet(sender, recipient,amount, idempotency_key, description)
+
+    return Response(
+        {
+            "reference": tx.reference,
+            "amount": tx.amount,
+            "status": tx.status,
+            "description": tx.description,
+            "created_at": tx.created_at
+        }, status=status.HTTP_201_CREATED
+    )
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def fund_wallet(request):
+    recipient = request.user.wallet
+
+    serializer = DepositSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    amount = serializer.validated_data['amount']
+
+    transaction = deposit(recipient, amount)
+
+    create_transfer_notification(recipient.user, amount)
+
+    return Response(
+        {
+            "reference": transaction.reference,
+            "amount": transaction.amount,
+            "status": transaction.status,
+            "description": transaction.description,
+            "created_at": transaction.created_at
+        }, status=status.HTTP_201_CREATED
+    )
+
+
