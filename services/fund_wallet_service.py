f17282c7c23a8050a3bdb0d406eba7b637279917
Truphile <salidmonreal@gmail.com>
2026-03-18 15:28:20 +0100

    pay commit

--- services/fund_wallet_service.py
+++ services/fund_wallet_service.py
@@ -1,4 +1,7 @@
+from wallet.services.fund_wallet import initiate_paystack_payment
+from notification.services import deposit_notification
+
 def fund_wallet(user, amount):
-    response=Initiate_paystack_payment(user,amount)
+    response=initiate_paystack_payment(user,amount)
     deposit_notification(user,amount)
     return response
