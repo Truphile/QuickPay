2227dbe7c0b33686724a3bcb590d3c5526bdd380
Truphile <salidmonreal@gmail.com>
2026-03-18 15:29:11 +0100

    pay commit

--- services/transfer_service.py
+++ services/transfer_service.py
@@ -2,7 +2,7 @@ from notification.services import create_transfer_notification
 from wallet.services.intra_transfer_service import transfer_wallet_to_wallet
 
 
-def create_transfer(sender, recipient, amount, idempotency_key, description=None)
+def create_transfer(sender, recipient, amount, idempotency_key, description=None):
     transaction = transfer_wallet_to_wallet(sender, recipient, amount, idempotency_key, description)
     create_transfer_notification(recipient.user, amount)
     return transaction
