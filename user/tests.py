68ce33c9e3908612627e3409b3ab481f38807d02
Truphile <salidmonreal@gmail.com>
2026-03-18 15:40:02 +0100

    pay commit

--- user/tests.py
+++ user/tests.py
@@ -62,3 +62,8 @@ class TestSignup(TestCase):
             self.client.post(self.url, self.data, format="json")
             response = self.client.post(self.login_url, self.login_data, format="json")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        def test_login_with_invalid_details_returns_400(self):
+            self.client.post(self.url, self.data, format="json")
+            response = self.client.post(self.login_url, self.login_data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
