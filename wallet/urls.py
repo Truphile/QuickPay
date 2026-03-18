015ec3424f188070d6265775032a78d0733e7ee7
Truphile <salidmonreal@gmail.com>
2026-03-18 15:44:34 +0100

    pay commit

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,4 +4,5 @@ from .views import transfer_wallet, fund_wallet
 urlpatterns = [
     path("transfer/",transfer_wallet,name="transfer")
     path('deposit/', fund_wallet, name="deposit")
+    path('callback/', paystack_callback, name="paystack_callback")
 ]
