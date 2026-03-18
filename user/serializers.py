ce90b97f7875e8671e98cd90f5bd04d8b34240b7
Truphile <salidmonreal@gmail.com>
2026-03-18 15:32:10 +0100

    pay commit

--- user/serializers.py
+++ user/serializers.py
@@ -12,10 +12,10 @@ class UserSerializer(serializers.ModelSerializer):
 
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create(**validated_data)
-        wallet.objects.create(owner=user, wallet_name=user.phone[1:])
-        return user
+    # def create(self, validated_data):
+    #     user = User.objects.create(**validated_data)
+    #     wallet.objects.create(owner=user, wallet_name=user.phone[1:])
+    #     return user
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
