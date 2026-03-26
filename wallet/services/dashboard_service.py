26c9747122aad84d0c7d94f633f82fe2ce00c780
Truphile <salidmonreal@gmail.com>
2026-03-27 00:24:54 +0100

    pay commit

---
+++ wallet/services/dashboard_service.py
@@ -0,0 +1,14 @@
+from wallet.models import Transaction
+
+
+def get_dashboard_data(user):
+    wallet = user.wallet
+    transactions = Transaction.objects.filter(sender=wallet).order_by('-created_at')[:5]
+    return {
+        "message": f"hi,{user.first_name}",
+        "wallet": wallet.wallet_number,
+        "transactions": transactions,
+        "balance": wallet.balance,
+        "status": wallet.status,
+        "currency": wallet.currency,
+    }
