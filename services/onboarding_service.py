32b59a93b16c886d2bd516badf72bddc3da77fd1
Truphile <salidmonreal@gmail.com>
2026-03-27 00:25:43 +0100

    pay commit

--- services/onboarding_service.py
+++ services/onboarding_service.py
@@ -1,8 +1,8 @@
 from django.db import transaction
 from user.services import create_user
-from wallet.services import create_wallet_service
+from wallet.services import create_wallet
 from notification.services import create_notification
-from wallet.services.create_wallet_service import create_wallet
+from wallet.services.create_wallet import create_wallet
 
 
 @transaction.atomic
